cd97368f549bbd3b4ca02aee91e0ba3c9ba9f475
Marcelo Henrique <marcelohpsousa444@gmail.com>
2025-09-10 16:18:26 -0300

    - Adição Serializador para atributo 'Status'; - Adição da notação @JsonIclude para atributos nulos;

--- src/main/java/fbd/ponto_eletronico/dto/EletronicPointsDTO.java
+++ src/main/java/fbd/ponto_eletronico/dto/EletronicPointsDTO.java
@@ -1,20 +1,26 @@
 package fbd.ponto_eletronico.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import fbd.ponto_eletronico.entity.EmployeesRoles;
-import fbd.ponto_eletronico.serialization.EmployeesRolesSerialize;
-import lombok.Data;
+import jakarta.persistence.Id;
+import serialize.StatusSerialize;
 
 import java.sql.Time;
 import java.time.LocalDate;
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public record EletronicPointsDTO(
+        @Id
+        Long id,
         LocalDate startDate,
         Time register_1,
         Time register_2,
         Time register_3,
         Time register_4,
         LocalDate endDate,
+
+        @JsonSerialize(using = StatusSerialize.class)
         Integer status,
-        @JsonSerialize(using = EmployeesRolesSerialize.class)
-        EmployeesRoles employeesRoles)  {}
+
+        @JsonIgnoreProperties({"id"}) EmployeesRolesDTO employeesRoles)  {}

9fcb66e0d47e6167fa5bacf6535c455ab89ddbb1
Marcelo Henrique <marcelohpsousa444@gmail.com>
2025-09-10 16:19:40 -0300

    - Adição de novos tipos de mapeamento;

--- src/main/java/fbd/ponto_eletronico/mapper/EletronicPointsMapper.java
+++ src/main/java/fbd/ponto_eletronico/mapper/EletronicPointsMapper.java
@@ -1,14 +1,27 @@
 package fbd.ponto_eletronico.mapper;
 
 import fbd.ponto_eletronico.dto.EletronicPointsDTO;
+import fbd.ponto_eletronico.dto.EmployeesRolesDTO;
 import fbd.ponto_eletronico.entity.EletronicPoints;
+import fbd.ponto_eletronico.entity.EmployeesRoles;
+import fbd.ponto_eletronico.request.EmployeesRolesPostRequest;
+import fbd.ponto_eletronico.request.EmployeesRolesPutRequest;
 import org.mapstruct.Mapper;
 
 import java.util.List;
 @Mapper(componentModel = "spring")
 public interface EletronicPointsMapper {
 
-    List<EletronicPointsDTO> eletronicPointsDto(List<EletronicPoints> eletronicPoints);
+    List<EletronicPointsDTO> toEletronicPointsDto(List<EletronicPoints> eletronicPoints);
 
+    List<EletronicPoints> toEletronicPoints(List<EletronicPointsDTO> eletronicPointsDTOS);
+
+    EletronicPointsDTO toEletronicPointsDto(EletronicPoints eletronicPoints);
+
+    EletronicPoints toEletronicPoints(EletronicPointsDTO eletronicPointsDTO);
+
+//    EletronicPoints toEmployeesRoles(EmployeesRolesPostRequest employeesRolesPostRequest);
+
+//    EletronicPoints toEmployeesRolesPut(EmployeesRolesPutRequest employeesRolesPutRequest);
 }
 

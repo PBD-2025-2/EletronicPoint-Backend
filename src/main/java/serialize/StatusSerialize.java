58ac6d9e26f9dfc680ccbfeed72ef92de634f00b
Joao.kjkj <jv157317@gmail.com>
2025-09-11 10:32:04 -0300

    Mudança na classe StatusSerialize: - Correção de atributo serializado (de EletronicPointsDTO para Integer status) - Adição de switch conversão de status int para String

--- src/main/java/serialize/StatusSerialize.java
+++ src/main/java/serialize/StatusSerialize.java
@@ -3,16 +3,23 @@ package serialize;
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.SerializerProvider;
-import fbd.ponto_eletronico.dto.EletronicPointsDTO;
-import fbd.ponto_eletronico.util.ParseStatusIntToStatusString;
 
 import java.io.IOException;
 
-public class StatusSerialize extends JsonSerializer<EletronicPointsDTO> {
+public class StatusSerialize extends JsonSerializer<Integer> {
     @Override
-    public void serialize(EletronicPointsDTO eletronicPointsDTO, JsonGenerator jsonGenerator, SerializerProvider serializerProvider) throws IOException {
-        jsonGenerator.writeStartObject();
-        jsonGenerator.writeStringField("status", ParseStatusIntToStatusString.ParseStatusToString(eletronicPointsDTO.status()));
-        jsonGenerator.writeEndObject();
+    public void serialize(Integer status, JsonGenerator jsonGenerator, SerializerProvider serializerProvider)
+            throws IOException {
+
+        String result = switch (status){
+
+            case 1 -> "Aberto";
+
+            case 2 -> "Fechado";
+
+            default -> "Pendente";
+        };
+        jsonGenerator.writeString(result);
+
     }
 }

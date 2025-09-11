0c4bd18adb4992c1400c7285406f38e045ea46cf
Joao.kjkj <jv157317@gmail.com>
2025-09-11 10:13:01 -0300

    Serialização adicionada para atributo workRegime

--- src/main/java/fbd/ponto_eletronico/dto/EmployeesRolesDTO.java
+++ src/main/java/fbd/ponto_eletronico/dto/EmployeesRolesDTO.java
@@ -1,10 +1,13 @@
 package fbd.ponto_eletronico.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import serialize.WorkRegimeSerialize;
 
 public record EmployeesRolesDTO (
         Long id,
         Boolean status,
+        @JsonSerialize(using = WorkRegimeSerialize.class)
         Integer workRegime,
         EmployeeEmbeddedDTO employee,
         @JsonIgnoreProperties({"id"}) RoleDTO role){}

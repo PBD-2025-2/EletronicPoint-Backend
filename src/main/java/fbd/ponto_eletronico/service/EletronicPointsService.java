e0543ce0f923b50c5c5fceec16d1a1ba58dc7913
Joao.kjkj <jv157317@gmail.com>
2025-09-12 16:17:01 -0300

    Criação de métodos save, replace e registerNewEletronicPoint

--- src/main/java/fbd/ponto_eletronico/service/EletronicPointsService.java
+++ src/main/java/fbd/ponto_eletronico/service/EletronicPointsService.java
@@ -3,17 +3,21 @@ package fbd.ponto_eletronico.service;
 import fbd.ponto_eletronico.dto.EletronicPointsDTO;
 import fbd.ponto_eletronico.dto.EmployeesRolesDTO;
 import fbd.ponto_eletronico.entity.EletronicPoints;
+import fbd.ponto_eletronico.entity.EmployeesRoles;
 import fbd.ponto_eletronico.exception.BadRequestException;
 import fbd.ponto_eletronico.mapper.EletronicPointsMapper;
 import fbd.ponto_eletronico.mapper.EmployeesRolesMapper;
 import fbd.ponto_eletronico.repository.EletronicPointsRepository;
+import fbd.ponto_eletronico.repository.EmployeesRolesRepository;
 import fbd.ponto_eletronico.request.EletronicPointsPostRequest;
+import fbd.ponto_eletronico.request.EletronicPointsPutRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 import java.util.Timer;
@@ -27,6 +31,7 @@ public class EletronicPointsService {
     private final EletronicPointsMapper eletronicPointsMapper;
     private final EmployeesRolesService employeesRolesService;
     private final EmployeesRolesMapper employeesRolesMapper;
+    private final EmployeesRolesRepository employeesRolesRepository;
 
     public List<EletronicPointsDTO> listAll() {
         List<EletronicPoints> eletronicPoints = eletronicPointsRepository.findAll();
@@ -79,23 +84,48 @@ public class EletronicPointsService {
         return eletronicPointsMapper.toEletronicPointsDto(eletronicPointsData);
     }
 
-    public EletronicPoints register(Long employeeRolesId){
-//        EmployeesRolesDTO employeesRolesData = employeesRolesService.findById(employeeRolesId);
+    public EletronicPointsDTO register(Long employeeRolesId){
         List<EletronicPoints> eletronicPointsListByEmployeesRoles = eletronicPointsRepository.findByEmployeesRoles_Id(employeeRolesId);
-        EletronicPoints eletronicPointsData = eletronicPointsListByEmployeesRoles.getLast();
-        if(eletronicPointsData != null){
-            return null;
-        }else {
+        if(eletronicPointsListByEmployeesRoles.isEmpty()) {
             return registerNewEletronicPoint(employeeRolesId);
         }
+        EletronicPoints eletronicPoints = eletronicPointsRepository.findByEmployeesRoles_Id(employeeRolesId).getLast();
+        List<LocalTime> registers = Arrays.asList(eletronicPoints.getRegister_2(), eletronicPoints.getRegister_3(), eletronicPoints.getRegister_4());
+        if(registers.get(0) == null){
+            eletronicPoints.setRegister_2(LocalTime.now());
+        }else if(registers.get(1) == null){
+            eletronicPoints.setRegister_4(LocalTime.now());
+        }
+
+        return eletronicPointsMapper.toEletronicPointsDto(eletronicPoints);
     }
 
-    private EletronicPoints registerNewEletronicPoint(Long employeeRolesId){
+    public EletronicPointsDTO replace(Long eletronicPointsId, EletronicPointsPutRequest eletronicPointsPutRequest){
+        EmployeesRoles employeesRolesData = employeesRolesRepository.getReferenceById(eletronicPointsPutRequest.employeeRolesId());
+        EletronicPoints eletronicPoints = eletronicPointsRepository.getReferenceById(eletronicPointsId);
+        EletronicPoints eletronicPointsReplaceDate = eletronicPointsMapper.eletronicPointsPutRequestToEletronicPoints(eletronicPointsPutRequest);
+        eletronicPointsReplaceDate.setId(eletronicPoints.getId());
+        eletronicPointsReplaceDate.setEmployeesRoles(employeesRolesData);
+
+        return eletronicPointsMapper.toEletronicPointsDto(eletronicPointsRepository.save(eletronicPointsReplaceDate));
+    }
+
+
+    private EletronicPointsDTO registerNewEletronicPoint(Long employeeRolesId){
+        EmployeesRoles employeesRolesData = employeesRolesMapper.toEmployeesRoles(employeesRolesService.findById(employeeRolesId));
         LocalDate dateNow = LocalDate.now();
-
-        EletronicPoints firstEletronicPoint = eletronicPointsMapper.toEletronicPoints(new EletronicPointsPostRequest(employeeRolesId, LocalDate.now(), LocalTime.now(), 1));
-
-        return firstEletronicPoint;
-
+        LocalTime timeNow = LocalTime.now();
+        EletronicPointsPostRequest eletronicPointsPostRequestData = new EletronicPointsPostRequest(employeesRolesData, dateNow, timeNow, 1);
+        EletronicPoints firstEletronicPoint = eletronicPointsMapper.toEletronicPoints(eletronicPointsPostRequestData);
+        return eletronicPointsMapper.toEletronicPointsDto(eletronicPointsRepository.save(firstEletronicPoint));
     }
+
+//    List<LocalTime> registers = Arrays.asList(eletronicPoints.getRegister_2(), eletronicPoints.getRegister_3(), eletronicPoints.getRegister_4());
+//        if(registers.get(0) == null){
+//        eletronicPoints.setRegister_2(LocalTime.now());
+//    }else if(registers.get(1) == null){
+//        eletronicPoints.setRegister_4(LocalTime.now());
+//    }
+
+
 }

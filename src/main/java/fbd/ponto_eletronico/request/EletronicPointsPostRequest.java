da2fd43938727f35aa784f07d65039a3deedbfaf
Joao.kjkj <jv157317@gmail.com>
2025-09-12 16:15:10 -0300

    Mudança de atributo employeeRolesId para employeeRoles.

--- src/main/java/fbd/ponto_eletronico/request/EletronicPointsPostRequest.java
+++ src/main/java/fbd/ponto_eletronico/request/EletronicPointsPostRequest.java
@@ -1,10 +1,12 @@
 package fbd.ponto_eletronico.request;
 
+import fbd.ponto_eletronico.entity.EmployeesRoles;
+
 import java.time.LocalDate;
 import java.time.LocalTime;
 
 public record EletronicPointsPostRequest(
-        Long employeesRolesId,
+        EmployeesRoles employeesRoles,
         LocalDate startDate,
         LocalTime register_1,
         Integer status) {}

b4c6cf93bd46f9858a96551d51257e2497a5892f
Dev.João <jv157317@gmail.com>
2025-09-18 16:19:39 -0300

    Criação de classe SecurityFilter para filtrar e fazer a verificação dos tokens.

---
+++ src/main/java/Auth_Microservice_PBD/Authentication/infra/security/SecurityFilter.java
@@ -0,0 +1,29 @@
+package Auth_Microservice_PBD.Authentication.infra.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+
+public class SecurityFilter extends OncePerRequestFilter {
+    private final TokenService tokenService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        var token = this.recoverToken(request);
+        if(token != null){
+            var username = t
+        }
+
+    }
+
+    private String recoverToken(HttpServletRequest request){
+        var authHeader = request.getHeader("Authorization");
+        if(authHeader == null) return null;
+        return authHeader.replace("Bearer ", "");
+    }
+}

a888e5cef1c9f7facb47f2f397395c374bd45d0a
Dev.João <jv157317@gmail.com>
2025-09-18 16:17:22 -0300

    Adição de método override loadUserByUsername

--- src/main/java/Auth_Microservice_PBD/Authentication/service/AuthService.java
+++ src/main/java/Auth_Microservice_PBD/Authentication/service/AuthService.java
@@ -3,16 +3,22 @@ package Auth_Microservice_PBD.Authentication.service;
 import Auth_Microservice_PBD.Authentication.dto.RegisterDTO;
 import Auth_Microservice_PBD.Authentication.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class AuthService {
-
+public class AuthService implements UserDetailsService {
 
     private final UserRepository userRepository;
 
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByUsername(username);
+    }
 
-    public RegisterDTO
+//    public RegisterDTO
 
 }

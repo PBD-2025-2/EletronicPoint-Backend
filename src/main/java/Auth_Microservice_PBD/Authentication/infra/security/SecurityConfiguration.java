603801722e2e29496302461ef81f68c44357d68a
Dev.João <jv157317@gmail.com>
2025-09-18 16:18:11 -0300

    Criação de classe SecurityConfiguration para geração e validação de token.

---
+++ src/main/java/Auth_Microservice_PBD/Authentication/infra/security/SecurityConfiguration.java
@@ -0,0 +1,34 @@
+package Auth_Microservice_PBD.Authentication.infra.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfiguration {
+
+    private final SecurityFilter securityFilter;
+
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
+        return httpSecurity
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(HttpMethod.POST, "users/").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.GET, "user/").hasRole("USER")
+                        .anyRequest().authenticated())
+                .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
+
+
+}

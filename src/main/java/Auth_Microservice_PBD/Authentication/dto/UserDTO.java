f65f6be14b4dcd207890c02bacf765614c6f3216
Dev.João <jv157317@gmail.com>
2025-09-20 15:31:20 -0300

    Update UserDTO.java
    - Retirada de atributo password

--- src/main/java/Auth_Microservice_PBD/Authentication/dto/UserDTO.java
+++ src/main/java/Auth_Microservice_PBD/Authentication/dto/UserDTO.java
@@ -2,4 +2,4 @@ package Auth_Microservice_PBD.Authentication.dto;
 
 import Auth_Microservice_PBD.Authentication.enums.UserRole;
 
-public record UserDTO(String name, String email, String username, String password) {}
+public record UserDTO(String name, String email, String username) {}

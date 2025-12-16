f8a881096f1da501e151e1f6ea38ff70e937ba3d
Marcelo Henrique <153032141+marceloHenriquePS@users.noreply.github.com>
2025-12-15 22:14:13 -0300

    - update(PointAdjustmentRequestDTO): Verification to avoid returning null data.

--- eletronicPoint/src/main/java/pbd/ponto_eletronico/dto/PointAdjustmentRequestDTO.java
+++ eletronicPoint/src/main/java/pbd/ponto_eletronico/dto/PointAdjustmentRequestDTO.java
@@ -1,6 +1,8 @@
 package pbd.ponto_eletronico.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import pbd.ponto_eletronico.enums.StatusType;
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public record PointAdjustmentRequestDTO(String justification, Object details, StatusType status, EletronicPointsDTO eletronicPoints) {
 }

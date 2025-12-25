e7fe95b6626d46968edc34e2fbfab67b1797bc13
Marcelo Henrique <153032141+marceloHenriquePS@users.noreply.github.com>
2025-12-24 21:51:43 -0300

    feat(company-dto): add id field to CompanyDTO

--- eletronicPoint/src/main/java/pbd/ponto_eletronico/dto/CompanyDTO.java
+++ eletronicPoint/src/main/java/pbd/ponto_eletronico/dto/CompanyDTO.java
@@ -4,6 +4,6 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.Id;
 
 public record CompanyDTO (
-        @Id Long id,
+        Long id,
         String name,
         @JsonIgnore String cnpj) {}
